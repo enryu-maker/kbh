9d88c0c1becf9787750c25859b1049042554fd73
enryu-maker <akifkhan60067@gmail.com>
2024-04-24 16:11:18 +0530

    cc

--- src/Helper/Helper.js
+++ src/Helper/Helper.js
@@ -1,5 +1,5 @@
 export const URLS = {
-    testing : "https://api-nerdtech.kbhjillhakrushisangh.org/",
+    testing : "https://apikbh.pythonanywhere.com/",
     // testing: "http://127.0.0.1:8000/",
-    imageurl:"https://api-nerdtech.kbhjillhakrushisangh.org",
+    imageurl:"https://apikbh.pythonanywhere.com",
 }
